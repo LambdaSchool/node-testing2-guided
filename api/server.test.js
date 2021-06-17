2f644b6521849a952183b4f26e37282652a8936c
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-06-17 19:38:27 +0200

    wip

--- api/server.test.js
+++ api/server.test.js
@@ -33,7 +33,6 @@ describe('[POST] /hobbit', () => {
   })
   it('returns newly created hobbit', async () => {
     const res = await request(server).post('/hobbits').send({ name: 'bilbo' })
-    expect(res.status).toBe(201);
     expect(res.data).toMatchObject({ id: res.data.id, name: 'bilbo' })
   })
 })
